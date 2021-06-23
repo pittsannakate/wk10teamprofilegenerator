65717ce37e17f6aa121764eddb4c34faf922b35e
pittsannakate <79388394+pittsannakate@users.noreply.github.com>
2021-06-22 22:17:54 -0400

    tests and json

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
-function sayHello(name) {
-    console.log('Hello ' + name)
-}
+const ManagerExport = require (./lib/Manager);
+const EngineerExport = require (./lib/Engineer);
+const InternExport = require (/lib/Intern);
+const inquirer = require("inquirer");
+const fs = require ("fs");
 
-sayHello ('Mosh');
+const employees = []
 
-setTimeout()
-clearTimeout()
+//Manager Function
+function createTeamMember() {
+    inquirer.prompt([{
+        type: "input",
+        name: "memberName",
+        message: "What is the team member's name?",
+        validate: answer => {
+            if (answer !== " ") {
+                return true;
+            }
+                return "Please enter a name!" 
+                },
+                type: "list"
+                message: "Select team member's role"
+                choices: [
+                    "Engineer",
+                    "Intern",
+                    "Manager",
+                ]
 
-setInterval()
-clearInterval()
+                }
+    }])
+};
 
-var sayHello = function() {
 
-}
-
-window.sayHello();
 
 
